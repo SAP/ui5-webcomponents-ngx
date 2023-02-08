2c423d6123a168a8f4c66e1b9407a43f0c65d86c
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2023-02-09 03:17:28 +0400

    fix: fixed ng-add schematic and ui5 theme loading (#30)

--- libs/ui5-angular/schematics/add-dependencies/index.ts
+++ libs/ui5-angular/schematics/add-dependencies/index.ts
@@ -1,8 +1,9 @@
-import { updateWorkspace } from '@schematics/angular/utility/workspace';
-import { Rule } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import { Schema } from '../schema';
-import { addPackageToPackageJson, getPackageVersionFromPackageJson } from '../utils/package-config';
+import {updateWorkspace} from '@schematics/angular/utility/workspace';
+import {Rule} from '@angular-devkit/schematics';
+import {NodePackageInstallTask} from '@angular-devkit/schematics/tasks';
+import {Schema} from '../schema';
+import {addPackageToPackageJson, getPackageVersionFromPackageJson} from '../utils/package-config';
+import packageJson from "../../package.json";
 
 export function addDependencies(options: Schema): Rule {
   return (tree, context) => updateWorkspace(async () => {
@@ -16,7 +17,15 @@ export function addDependencies(options: Schema): Rule {
     }
 
     if (options.theming && !getPackageVersionFromPackageJson(tree, '@ui5/theming-ngx')) {
-      addPackageToPackageJson(tree, '@ui5/theming-ngx', 'latest');
+      addPackageToPackageJson(tree, '@ui5/theming-ngx', packageJson.version);
+    }
+
+    const peerDependencies = Object.keys(packageJson.peerDependencies as Record<string, string>).map((peerDependency: string): [string, string] => ([peerDependency, packageJson.peerDependencies[peerDependency]]));
+
+    for (const [peerDependency, version] of peerDependencies) {
+      if (!getPackageVersionFromPackageJson(tree, peerDependency)) {
+        addPackageToPackageJson(tree, peerDependency, version);
+      }
     }
 
     context.addTask(new NodePackageInstallTask());

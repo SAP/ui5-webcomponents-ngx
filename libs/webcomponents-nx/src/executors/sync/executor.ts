96651135df838a36478a5388c0e424943c0706be
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-02-18 01:27:02 +0400

    feat: added support for multiple configs

--- libs/webcomponents-nx/src/executors/sync/executor.ts
+++ libs/webcomponents-nx/src/executors/sync/executor.ts
@@ -4,18 +4,27 @@ import {flushChanges, FsTree, printChanges} from "nx/src/generators/tree";
 import {workspaceRoot} from "nx/src/utils/app-root";
 
 export default async function (
-  schema: { configFile: string },
+  schema: { configFile?: string, configFiles?: string[] },
   executorContext: ExecutorContext
 ): Promise<{ success: boolean }> {
-  const {default: wrapperConfig} = await import(schema.configFile);
+  if (!schema.configFile && !schema.configFiles) {
+    throw new Error('No config file specified');
+  }
   const host = new FsTree(workspaceRoot, executorContext.isVerbose);
-  const task = await generateComponents(host, {...wrapperConfig, project: executorContext.projectName});
+  const configFiles = schema.configFile ? [schema.configFile] : schema.configFiles;
+  const tasks = [];
+  for (const configFile of configFiles) {
+    const {default: wrapperConfig} = await import(configFile);
+    tasks.push(await generateComponents(host, {...wrapperConfig, project: executorContext.projectName}));
+  }
   const changes = host.listChanges();
   printChanges(changes);
 
   flushChanges(workspaceRoot, changes);
-  if (task) {
-    await task();
+  if (tasks.length > 0) {
+    for (const task of tasks) {
+      await task();
+    }
   }
   return {success: true};
 }

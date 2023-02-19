46298b0f47eb8a4d2d318676c6fe677abbd2a92a
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-02-20 01:59:17 +0400

    feat: added jest snapshot testing of the output files

---
+++ libs/ui5-angular/jest.config.ts
@@ -0,0 +1,21 @@
+/* eslint-disable */
+export default {
+  displayName: 'ui5-angular',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/libs/ui5-angular',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};

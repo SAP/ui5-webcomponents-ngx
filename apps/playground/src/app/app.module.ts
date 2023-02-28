d8670d8d2d40e5ccc3f74ea29ea1975f22a187f2
g-cheishvili <giorgi.cheishvili@outlook.com>
2023-02-28 14:15:56 +0400

    feat: added i18n pipe and service

--- apps/playground/src/app/app.module.ts
+++ apps/playground/src/app/app.module.ts
@@ -10,18 +10,44 @@ import {Ui5WebcomponentsThemingModule} from "@ui5/webcomponents-ngx/theming";
 import {Ui5WebcomponentsIconsModule} from "@ui5/webcomponents-ngx/icons";
 import {Ui5WebcomponentsModule} from '@ui5/webcomponents-ngx';
 import {Ui5WebcomponentsConfigModule} from '@ui5/webcomponents-ngx/config';
+import {Ui5I18nModule} from "@ui5/webcomponents-ngx/i18n";
+import {RouterModule} from "@angular/router";
+import {MainComponent} from "./main.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainComponent],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     Ui5ThemingModule.forRoot({defaultTheme: 'sap_fiori_3'}),
     Ui5WebcomponentsIconsModule.forRoot(['sap-icons', 'tnt-icons', "business-suite-icons"]),
-    Ui5WebcomponentsConfigModule.forRoot({
+    Ui5WebcomponentsConfigModule.forRoot({}),
+    Ui5I18nModule.forRoot({
       language: 'ru',
+      fetchDefaultLanguage: true,
+      bundle: {
+        name: 'i18n_root',
+        translations: {
+          en: fetch('assets/i18n/messages_en').then(r => r.text()),
+          ru: fetch('assets/i18n/messages_ru').then(r => r.text())
+        }
+      }
     }),
+    RouterModule.forRoot([
+      {
+        path: '', component: AppComponent, children: [
+          {
+            path: '',
+            component: MainComponent
+          },
+          {
+            path: 'child-module',
+            loadChildren: () => import('./modules/child/child.module').then(m => m.ChildModule)
+          }
+        ]
+      },
+    ]),
     Ui5WebcomponentsModule,
     Ui5WebcomponentsThemingModule.forRoot(),
     FundamentalStylesComponentsModule,
---
+++ libs/ui5-angular/i18n/i18n.pipe.ts
@@ -0,0 +1,46 @@
+import {ChangeDetectorRef, OnDestroy, Pipe, PipeTransform} from "@angular/core";
+import {I18nService} from "./i18n.service";
+import {distinctUntilChanged, Subject, Subscription, takeUntil} from "rxjs";
+
+@Pipe({
+  name: 'ui5I18n',
+  pure: false
+})
+export class I18nPipe implements PipeTransform, OnDestroy {
+  private _lastTranslationValue = '';
+  private _subscription?: Subscription;
+  private _destroyed$ = new Subject<void>();
+  private _lastValue!: string;
+  private _lastValueParams!: string[];
+
+  constructor(private changeDetectorRef: ChangeDetectorRef,
+              private i18nService: I18nService) {
+  }
+
+  transform(value: string, ...params: any[]): string {
+    const paramsChanged = !this._lastValue || params.some((param, index) => param !== this._lastValueParams[index]);
+    if (value === this._lastValue && !paramsChanged) {
+      return this._lastTranslationValue;
+    }
+    this._lastValue = value;
+    this._lastValueParams = params;
+    if (this._subscription) {
+      this._subscription.unsubscribe();
+      this._subscription = undefined;
+    }
+    this._subscription = this.i18nService.getText(value, ...params)
+      .pipe(
+        distinctUntilChanged(),
+        takeUntil(this._destroyed$)
+      ).subscribe((text) => {
+        this._lastTranslationValue = text;
+        this.changeDetectorRef.markForCheck();
+      });
+    return this._lastTranslationValue;
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed$.next();
+    this._destroyed$.complete();
+  }
+}

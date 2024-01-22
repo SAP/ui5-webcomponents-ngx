f879ebd28f34981e133ed58d64df13cbba3cd903
Giorgi Cheishvili <giorgi.cheishvili@outlook.com>
2024-01-23 01:53:18 +0400

    fix: build

--- apps/documentation/api-json.config.ts
+++ apps/documentation/api-json.config.ts
@@ -1,13 +1,13 @@
 import {ComponentData, GeneratedFile, TransformerConfig} from "@ui5/webcomponents-transformer";
-import { apiJsonParser } from '@ui5/webcomponents-schema-parser';
+import { customElementSchemaParser } from '@ui5/webcomponents-schema-parser';
 import {stringify} from 'flatted';
 
 export default {
-  gatherer: () => apiJsonParser({
+  gatherer: () => customElementSchemaParser({
     sources: [
-      '@ui5/webcomponents-base/dist/api.json',
-      '@ui5/webcomponents-fiori/dist/api.json',
-      '@ui5/webcomponents/dist/api.json',
+      '@ui5/webcomponents-base/dist/custom-elements-internal.json',
+      '@ui5/webcomponents-fiori/dist/custom-elements-internal.json',
+      '@ui5/webcomponents/dist/custom-elements-internal.json'
     ]
   }),
   transformers: [

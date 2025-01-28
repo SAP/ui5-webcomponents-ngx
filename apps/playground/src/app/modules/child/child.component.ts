ecc364f9126bff9f11006ed51ba6f4cb13c76c34
droshev <mladen.droshev@sap.com>
2025-01-28 15:00:38 -0500

    feat: angular 19 support BREAKING CHANGE: angular 19 support

--- apps/playground/src/app/modules/child/child.component.ts
+++ apps/playground/src/app/modules/child/child.component.ts
@@ -2,10 +2,11 @@ import {Component, inject} from '@angular/core';
 import {I18nService} from "@ui5/webcomponents-ngx/i18n";
 
 @Component({
-  template: `
+    template: `
     <ui5-button routerLink="/">{{ 'SOMETHING' | ui5I18n : 'text from the child' }}</ui5-button>
     <ui5-button (click)="changeLanguage()">{{ 'CHANGE_LANGUAGE' | ui5I18n }}</ui5-button>
-  `
+  `,
+    standalone: false
 })
 export class ChildComponent {
   i18nService = inject(I18nService);

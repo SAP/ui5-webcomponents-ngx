314470f9af6e522b60b060a92d55746207ddf1f9
droshev <mladen.droshev@sap.com>
2025-02-24 18:44:20 -0500

    docs: use description from json schema

--- apps/documentation/src/app/arg-type-tools.ts
+++ apps/documentation/src/app/arg-type-tools.ts
@@ -1,63 +1,41 @@
 import {parse} from "flatted";
 import flattedJson from "../api-json.json";
 import {ArgTypes} from "@storybook/angular";
+import { reflectComponentType } from "@angular/core";
 const apiJson = parse(JSON.stringify(flattedJson)).reduce((acc, next) => {
-  acc[next.baseName] = next;
+  acc[next.baseName + '/' + next.selector] = next;
   return acc;
 }, {});
 
 export const extractArgTypes = (component: { name: string }): ArgTypes => {
   const componentName = component.name.replace('Component', '');
-  const argTypes = {};
+  const argTypes: Record<string, any> = {};
+  const idName = componentName + '/' + reflectComponentType(component as any)?.selector;
 
-  apiJson[componentName]?.inputs.forEach((input) => {
-    argTypes[input.name] = {
-      type: input.type,
-      description: input.description,
-      name: input.name,
-      defaultValue: input.defaultValue,
-      table: {
-        category: 'Inputs',
-      },
-      control: false
-    };
-  }, {});
-
-  apiJson[componentName]?.outputs.forEach((output) => {
-    argTypes[output.name] = {
-      type: output.type,
-      description: output.description,
-      name: output.name,
-      table: {
-        category: 'Outputs',
-      },
-      control: false
-    };
-  });
-
-  apiJson[componentName]?.slots.forEach((slot) => {
-    argTypes[slot.name] = {
-      type: slot.type,
-      description: slot.description,
-      name: slot.name,
-      table: {
-        category: 'Slots',
-      },
-      control: false
-    };
-  });
-
-  apiJson[componentName]?.methods.forEach((property) => {
-    argTypes[property.name] = {
-      type: property.type,
-      description: property.description,
-      name: property.name,
-      table: {
-        category: 'Methods',
-      },
-      control: false
-    };
+  const categories = {
+    inputs: 'Inputs',
+    outputs: 'Outputs',
+    slots: 'Slots',
+    methods: 'Methods',
+  };
+  
+  Object.entries(categories).forEach(([key, category]) => {
+    apiJson[idName]?.[key]?.forEach((item) => {
+      argTypes[item.name] = {
+        type: item.type,
+        description: item.description,
+        name: item.name,
+        table: { category },
+        control: false,
+      };
+    });
   });
 
   return argTypes;
+}
+
+export const extractDescription = (name: string, component: any, additionalDescription: string ): string => {
+  const componentName = name.replace('Component', '');
+  const idName = componentName + '/' + reflectComponentType(component as any)?.selector;
+  return apiJson[idName]?.description + additionalDescription;
 }
---
+++ apps/documentation/src/app/stories/ai/button.stories.ts
@@ -0,0 +1,45 @@
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+
+import { ButtonComponent } from '@ui5/webcomponents-ngx/ai/button';
+import { ButtonStateComponent } from '@ui5/webcomponents-ngx/ai/button-state';
+import { MenuComponent } from '@ui5/webcomponents-ngx/main/menu';
+import { MenuItemComponent } from '@ui5/webcomponents-ngx/main/menu-item';
+import { extractArgTypes, extractDescription } from '../../arg-type-tools';
+import { AiButtonComponent } from './ai-button.component';
+
+const additionalDescription = `
+
+<h3>Module Import</h3>
+
+<code>import { ButtonComponent } from "@ui5/webcomponents-ngx/ai/button";</code>`
+
+export default {
+  title: 'UI5 Web Components / AI / Button',
+  component: AiButtonComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [
+        ButtonComponent, 
+        ButtonStateComponent,
+        MenuComponent,
+        MenuItemComponent,
+        AiButtonComponent],
+    }),
+  ],
+  parameters: {
+    docs: {
+		extractArgTypes,
+      description: {
+        component: extractDescription('ButtonComponent', ButtonComponent, additionalDescription ),
+      },
+    },
+  },
+} as Meta;
+
+export const DefaultButton: StoryObj<AiButtonComponent> = {
+  render: (args) => ({
+    props: args,
+    template: `<app-ai-button></app-ai-button>`
+  }),
+};
+

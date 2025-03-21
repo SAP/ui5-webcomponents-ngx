aa9d9fa64d21e7ddf81abf64bcb1a2726f97eee5
Hristov <alex-hristov@hotmail.com>
2025-03-21 10:03:32 -0400

    docs: added more examples to the label, link, list and multicombobox components

--- apps/documentation/src/app/stories/main/link.stories.ts
+++ apps/documentation/src/app/stories/main/link.stories.ts
@@ -20,7 +20,11 @@ export default {
     docs: {
       extractArgTypes,
       description: {
-        component: extractDescription('LinkComponent', LinkComponent, description),
+        component: extractDescription(
+          'LinkComponent',
+          LinkComponent,
+          description,
+        ),
       },
     },
   },
@@ -35,5 +39,58 @@ export const differentLinkDesigns: StoryObj<LinkComponent> = {
 		<ui5-link class="samples-big-margin-right" href="https://www.sap.com" target="_blank" disabled>Disabled</ui5-link>
 		<ui5-link class="samples-big-margin-right" href="https://www.sap.com" target="_blank" design="Emphasized">Emphasized</ui5-link>
 	`,
+  }),
+};
+
+export const TextTruncationAndWrapping: StoryObj<LinkComponent> = {
+  render: (args: LinkComponent & any) => ({
+    props: args,
+    template: `
+    <ui5-link href="https://www.sap.com" target="_blank" style="width: 8rem">
+        This is a really long link. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </ui5-link>
+
+    <ui5-link href="https://www.sap.com" target="_blank" wrapping-type="None" style="width: 8rem">
+        This is a really long link. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </ui5-link>
+  `,
+  }),
+};
+
+export const CustomStylingLink: StoryObj<LinkComponent> = {
+  render: (args: LinkComponent & any) => ({
+    props: args,
+    template: `
+    <ui5-link
+        href="https://www.sap.com"
+        target="_blank"
+        style="font-size: 1.25rem; color: var(--sapNegativeColor);"
+    >Styled Link
+    </ui5-link>
+
+    <ui5-link
+        href="https://www.sap.com"
+        target="_blank"
+        style="font-size: 1.25rem; color: var(--sapPositiveColor);"
+    >Styled Link
+    </ui5-link>
+  `,
+  }),
+};
+
+export const LinkWithIcon: StoryObj<LinkComponent> = {
+  render: (args: LinkComponent & any) => ({
+    props: args,
+    template: `
+    <ui5-link href="https://www.sap.com" target="_blank" icon="employee">
+        View Profile
+    </ui5-link>
+
+    <br>
+
+    <ui5-link href="https://www.sap.com" target="_blank" wrapping-type="Normal" end-icon="cloud">
+        Check the weather
+    </ui5-link>
+  `,
   }),
 };
